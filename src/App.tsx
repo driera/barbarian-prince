00a585a9365509ee5d05c1a33cb3683662cb821e
Dani Riera <daniel.riera@qatium.com>
2021-12-19 23:49:34 +0100

    Add basic stats

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import classes from "./App.module.css";
 function App() {
   return (
     <div className={classes.app}>
-      <h1>Barbarian Prince game sheet</h1>{" "}
+      <h1>Barbarian Prince game sheet</h1>
+      <div>
+        <ul>
+          <li>Combat: 8</li>
+          <li>Endurance: 9</li>
+          <li>Wit & wiles: 5</li>
+          <li>Gold: 1</li>
+          <li>Food: 3</li>
+        </ul>
+      </div>
     </div>
   );
 }
